ad1227be4b0d01ff732838027af31cd8700b6622
Simon  Flachs <Simon@simons-air-4.noko.local>
2015-10-22 21:52:53 +0200

    added feature engineering

--- analysis.py
+++ analysis.py
@@ -243,4 +243,20 @@ print(scores.mean())
 
 
 
+#feature engineering
+from sklearn.feature_selection import SelectKBest, f_classif
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Perform feature selection
+selector = SelectKBest(f_classif, k=5)
+selector.fit(star_wars[predictors], star_wars["IsStarWarsFan"])
+
+# Get the raw p-values for each feature, and transform from p-values into scores
+scores = -np.log10(selector.pvalues_)
+
+# Plot the scores.  See how "Pclass", "Sex", "Title", and "Fare" are the best?
+plt.bar(range(len(predictors)), scores)
+plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+plt.show()
 
